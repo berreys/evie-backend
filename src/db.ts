4a569dc77edaf837e68fcdcd0d266d370098f6a8
berreys <saberrey@gmail.com>
2024-10-08 14:49:09 -0600

    getUser functionality ... TODO: add login endpoing utilizing this

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import { InsertOneResult, MongoClient, ObjectId } from "mongodb";
+import { InsertOneResult, WithId, MongoClient, ObjectId } from "mongodb";
 import { User } from './types'
 const config = require('../dbconfig.json');
 const url: string = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
@@ -12,10 +12,31 @@ export async function addUser(user: User): Promise<string | null> {
         const collection = database.collection('users');
         const result: InsertOneResult = await collection.insertOne(user);
         return result.insertedId.toString();
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error adding user:', error);
         return null;
-    } finally {
+    }
+    finally {
+        await client.close();
+    }
+}
+
+// Gets user from database
+export async function getUser(username: string): Promise<User | null> {
+    try {
+        await client.connect();
+        const database = client.db('users');
+        const collection = database.collection('users');
+        const result = await collection.findOne({username: username});
+        console.log(result);
+        return null;
+    }
+    catch (error) {
+        console.error("Error retrieving user:', error");
+        return null;
+    }
+    finally {
         await client.close();
     }
 }
