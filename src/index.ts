de80f699f6dfbe98e2683d23ca5a6c16b7dde7ff
berreys <saberrey@gmail.com>
2024-10-09 14:40:51 -0600

    cors fix

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500'
+    origin: true
 }));
 import { Request, Response } from 'express';
 import dotenv from 'dotenv';
