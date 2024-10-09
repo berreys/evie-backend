9b73caddf24d02e4e21b6e14a617ede1244e264e
berreys <saberrey@gmail.com>
2024-10-09 13:45:59 -0600

    add env vars

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,24 @@ import { Request, Response } from 'express';
 const port = process.env.PORT || 3000;
 
 // Dynamically load each route in ./routes
-// const loadRoutes = (app: express.Application) => {
-//     const routesPath = path.join(__dirname, 'routes');
+const loadRoutes = (app: express.Application) => {
+    const routesPath = path.join(__dirname, 'routes');
   
-//     // Read all files from the routes directory
-//     readdirSync(routesPath).forEach((file) => {
-//         const route = path.join(routesPath, file);
+    // Read all files from the routes directory
+    readdirSync(routesPath).forEach((file) => {
+        const route = path.join(routesPath, file);
     
-//         // Dynamically import each route and use it
-//         const routeModule = require(route).default;
-//         const routeName = `/${file.split('.')[0]}`;
+        // Dynamically import each route and use it
+        const routeModule = require(route).default;
+        const routeName = `/${file.split('.')[0]}`;
     
-//         // Each route is the name of the file (eg. health.ts -> '/health')
-//         app.use(routeName, routeModule);
-//     });
-// };
+        // Each route is the name of the file (eg. health.ts -> '/health')
+        app.use(routeName, routeModule);
+    });
+};
 
 // Call the function to load routes
-// loadRoutes(app);
+loadRoutes(app);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Evie\nConnecting EV drivers to thousands of chargers around the country.');
