0e843cb82c4c9d9ee085f4e9aa65343938537faa
berreys <saberrey@gmail.com>
2024-10-09 14:15:36 -0600

    readd cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { readdirSync } from 'fs';
 import path from 'path';
-// import cors from 'cors';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 import { Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
