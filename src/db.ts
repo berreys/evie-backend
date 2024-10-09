820841f1db9223a01ac5aab8c798d4dad21711ff
berreys <saberrey@gmail.com>
2024-10-09 13:40:07 -0600

    test by commenting out db code

--- src/db.ts
+++ src/db.ts
@@ -1,42 +1,42 @@
-import { InsertOneResult, WithId, MongoClient, ObjectId } from "mongodb";
-import { User } from './types'
-const config = require('../dbconfig.json');
-const url: string = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
-const client: MongoClient = new MongoClient(url);
+// import { InsertOneResult, WithId, MongoClient, ObjectId } from "mongodb";
+// import { User } from './types'
+// const config = require('../dbconfig.json');
+// const url: string = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
+// const client: MongoClient = new MongoClient(url);
 
-// Adds user to database
-export async function addUser(user: User): Promise<string | null> {
-    try {
-        await client.connect();
-        const database = client.db('users');
-        const collection = database.collection('users');
-        const result: InsertOneResult = await collection.insertOne(user);
-        return result.insertedId.toString();
-    }
-    catch (error) {
-        console.error('Error adding user:', error);
-        return null;
-    }
-    finally {
-        await client.close();
-    }
-}
+// // Adds user to database
+// export async function addUser(user: User): Promise<string | null> {
+//     try {
+//         await client.connect();
+//         const database = client.db('users');
+//         const collection = database.collection('users');
+//         const result: InsertOneResult = await collection.insertOne(user);
+//         return result.insertedId.toString();
+//     }
+//     catch (error) {
+//         console.error('Error adding user:', error);
+//         return null;
+//     }
+//     finally {
+//         await client.close();
+//     }
+// }
 
-// Gets user from database
-export async function getUser(username: string): Promise<User | null> {
-    try {
-        await client.connect();
-        const database = client.db('users');
-        const collection = database.collection('users');
-        const result = await collection.findOne({username: username});
-        console.log(result);
-        return null;
-    }
-    catch (error) {
-        console.error("Error retrieving user:', error");
-        return null;
-    }
-    finally {
-        await client.close();
-    }
-}
+// // Gets user from database
+// export async function getUser(username: string): Promise<User | null> {
+//     try {
+//         await client.connect();
+//         const database = client.db('users');
+//         const collection = database.collection('users');
+//         const result = await collection.findOne({username: username});
+//         console.log(result);
+//         return null;
+//     }
+//     catch (error) {
+//         console.error("Error retrieving user:', error");
+//         return null;
+//     }
+//     finally {
+//         await client.close();
+//     }
+// }
