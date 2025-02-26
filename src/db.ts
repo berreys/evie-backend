ed2731e78c0e57d859c2a39e0881f220b7de12ac
berreys <saberrey@gmail.com>
2025-02-26 14:41:22 -0700

    register endpoint now populates account, vehicleowner, chargerowner tables

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import { User } from './types';
-import sql, { QueryOptions, QueryResult, ResultSetHeader } from 'mysql2';
+import sql, { QueryOptions, QueryResult, ResultSetHeader } from 'mysql2/promise';
 
 const config = {
     user: process.env.db_username,
@@ -8,54 +8,31 @@ const config = {
     host: process.env.db_server
 };
 
-async function executeQuery(query: QueryOptions) : Promise<QueryResult | null> {
-    let connection = null;
-    let res = null;
-    try {
-        connection = await sql.createConnection(config);
-        connection.connect((err) => {
-            if(err){
-                console.error('Error connecting to database:', err.message);
-                return;
-            }
-            console.log('Connected to database.');
-        })
-        connection.query(query, (err, results) => {
-            if(err){
-                console.error('Error executing query:', err.message);
-                return;
-            }
-            console.log('Query results:', results);
-            res = results;
-        })
-    }
-    catch (error) {
-        console.error('Error adding user:', error);
-    }
-    finally {
-        if(connection){
-            await connection.end();
-            console.log('Database connection closed.');
-        }
-        return res;
-    }
-}
-
 // Adds user to database
 export async function addUser(user: User) {
     let connection = null;
     let res = null;
     try {
         connection = await sql.createConnection(config);
-        console.log('Connected to database.');
 
         let query = 'INSERT INTO account (firstName, lastName, username, passwordHashed, email, createdAt) VALUES (?, ?, ?, ?, ?, NOW());';
         let values = [user.firstName, user.lastName, user.username, user.password, user.email]
         const insertRes: any = await connection.execute(query, values);
 
         query = 'SELECT * FROM account WHERE id = ?;';
-        values = [insertRes.insertId]
+        values = [insertRes[0].insertId];
         const rows: any = await connection.execute(query, values);
+
+        if(user.chargerOwner){
+            query = 'INSERT INTO chargerowner (accountId) VALUES (?);';
+            values = [insertRes[0].insertId];
+            await connection.execute(query, values);
+        }
+        if(user.vehicleOwner){
+            query = 'INSERT INTO vehicleowner (accountId) VALUES (?);';
+            values = [insertRes[0].insertId];
+            await connection.execute(query, values);
+        }
 
         res = rows[0];
 
