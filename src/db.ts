d882834cec446a4d1ae6416d335b3eed63353b66
berreys <saberrey@gmail.com>
2025-02-26 15:29:51 -0700

    add comments

--- src/db.ts
+++ src/db.ts
@@ -15,19 +15,24 @@ export async function addUser(user: User) {
     try {
         connection = await sql.createConnection(config);
 
+        // Insert user into account table
         let query = 'INSERT INTO account (firstName, lastName, username, passwordHashed, email, createdAt) VALUES (?, ?, ?, ?, ?, NOW());';
         let values = [user.firstName, user.lastName, user.username, user.password, user.email]
         const insertRes: any = await connection.execute(query, values);
 
+        // Retrieve user's row from account table that we just inserted
         query = 'SELECT * FROM account WHERE id = ?;';
         values = [insertRes[0].insertId];
         const rows: any = await connection.execute(query, values);
 
+        // Insert user's ID into chargerowner table if they own a charger
         if(user.chargerOwner){
             query = 'INSERT INTO chargerowner (accountId) VALUES (?);';
             values = [insertRes[0].insertId];
             await connection.execute(query, values);
         }
+
+        // Insert user's ID into vehicleowner if they own a vehicle
         if(user.vehicleOwner){
             query = 'INSERT INTO vehicleowner (accountId) VALUES (?);';
             values = [insertRes[0].insertId];
